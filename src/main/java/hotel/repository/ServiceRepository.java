0d4d010f89964b748ec0647c423ddd19f4e6c786
Majjkell <mikolajzeberkiewicz@gmail>
2020-04-28 16:30:17 +0200

    Deleted not used files

--- src/main/java/hotel/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package hotel.repository;
-
-import hotel.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<Service,Long> {
-}
