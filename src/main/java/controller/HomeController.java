1660a96e02d682f7d4fb7c9271851022bfb3f0d5
Mikołaj Żeberkiewicz <mikolajzeberkiewicz@gmail.com>
2020-05-02 14:06:00 +0200

    m

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping("/")
+//    @RequestMapping("/")
     public String displayMainPage(){
         return "index";
     }
